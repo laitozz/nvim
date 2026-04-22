cb73925644bd8b07e5d7d982f5c281b98436663d
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-04-22 17:54:47 +0300

    Update org config

--- lua/plugins/org.lua
+++ lua/plugins/org.lua
@@ -8,8 +8,10 @@ return {
 	config = function()
 		-- Setup orgmode
 		require('orgmode').setup({
-			org_agenda_files = { '~/org/**/*' },
+			org_agenda_files = { '~/org/*' },
 			org_default_notes_file = '~/org/refile.org',
+			org_todo_keywords = { 'TODO(t)', '[ ](c)', '|', '[X](D)', 'DONE(d)' },
+			win_split_mode = 'float',
 			org_hide_leading_stars = true,
 			org_hide_emphasis_markers = true,
 			org_startup_indented = true,
