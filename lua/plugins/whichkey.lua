987463c4446b9c181f7d7c39cd79cccb6217da72
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-05-09 12:33:36 +0300

    Hide grapple binds in whichkey

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -30,6 +30,17 @@ return {
 					{ "<leader>im", icon = "󰟾 "},
 					{ "<leader>T", icon = " "},
 
+					-- Hide grapple 
+					{ "<leader>1", hidden = true },
+					{ "<leader>2", hidden = true },
+					{ "<leader>3", hidden = true },
+					{ "<leader>4", hidden = true },
+					{ "<leader>5", hidden = true },
+					{ "<leader>6", hidden = true },
+					{ "<leader>7", hidden = true },
+					{ "<leader>8", hidden = true },
+					{ "<leader>9", hidden = true },
+
 				},
 			},
 
