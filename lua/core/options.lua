55485ddfc2f1e403596b2320ef9910c4396335a7
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-06-04 21:00:05 +0300

    Do not use system clipboard

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,6 @@
 local opts = {
 	backup = false,                          -- creates a backup file
-	clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
+	-- clipboard = "unnamedplus",               -- allows neovim to access the system clipboard
 	cmdheight = 2,                           -- more space in the neovim command line for displaying messages
 	completeopt = { "menuone", "noselect" }, -- mostly just for cmp
 	conceallevel = 2,                        -- so that `` is visible in markdown files
