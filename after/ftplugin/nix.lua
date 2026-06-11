42c43044f0b7b13df4b85be1bdd453d357d27e5b
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-06-12 00:09:35 +0300

    Update nix ftplugin

--- after/ftplugin/nix.lua
+++ after/ftplugin/nix.lua
@@ -1,3 +1,4 @@
-vim.opt_local.shiftwidth = 2
-vim.opt_local.tabstop = 2
-vim.opt_local.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.tabstop = 2
+vim.bo.expandtab = true
+vim.bo.smartindent = false
