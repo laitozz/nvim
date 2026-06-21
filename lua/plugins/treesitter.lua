56103da43053f4f2a9219f701bbe1e527597598f
Oiva Laitinen <oiva.laitinen2@gmail.com>
2026-06-21 19:46:33 +0300

    Change to arborist.nvim for treesitter
    This should enable TS indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,5 @@
-local config = function()
-	require("tree-sitter-manager").setup({
-		-- Default Options
-		ensure_installed = {}, -- list of parsers to install at the start of a neovim session
-		border = nil, -- border style for the window (e.g. "rounded", "single"), if nil, use the default border style defined by 'vim.o.winborder'. See :h 'winborder' for more info.
-		auto_install = true, -- if enabled, install missing parsers when editing a new file
-		highlight = false, -- treesitter highlighting is enabled by default
-	})
-	-- NOTE: workaround for
-	-- https://github.com/romus204/tree-sitter-manager.nvim/issues/54
-	vim.api.nvim_create_autocmd('FileType', {
-		callback = function()
-			pcall(vim.treesitter.start)
-		end,
-	})
-end
-
 return {
-	"romus204/tree-sitter-manager.nvim",
-	dependencies = {}, -- tree-sitter CLI must be installed system-wide
+	"arborist-ts/arborist.nvim",
 	lazy = false,
-	keys = {{ "<leader>it", "<cmd>TSManager<cr>", desc = "Tree Sitter TUI"}},
-	cmd = { "TSManager" },
-	config = config,
+	opts = true,
 }
